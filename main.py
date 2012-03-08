255ecdd43ffe9815099a5a7d9130203f6d6b6808
JesseTyler <jessetyler@jessetyler.net>
2012-03-08 15:08:58 -0800

    Tickan, updatan, movan

--- main.py
+++ main.py
@@ -1,42 +1,54 @@
 from __future__ import print_function
 
-import pyglet
+import pyglet, math
 from pyglet.window import *
 
-window = pyglet.window.Window()
-image = pyglet.resource.image('img/crate.png')
+window = pyglet.window.Window(600,600)
 
-image_x = 0
-image_y = 0
+tick = 0
 
-@window.event
-def on_key_press(symbol, modifiers):
-    global image_x, image_y
-    if symbol == key.W:
-        if image_y + 40 > 440:
-            return
-        image_y += 40
-        print('y: %d x: %d' % (image_y, image_x))
-    elif symbol == key.S:
-        if image_y - 40 < 0:
-            return
-        image_y -= 40
-        print('y: %d x: %d' % (image_y, image_x))
-    elif symbol == key.A:
-        if image_x - 40 < 0:
-            return
-        image_x -= 40
-        print('y: %d x: %d' % (image_y, image_x))
-    elif symbol == key.D:
-        if image_x + 40 > 600:
-            return
-        image_x += 40
-        print('y: %d x: %d' % (image_y, image_x))
-                          
+keys = key.KeyStateHandler()
+window.push_handlers(keys)
+
+crate_img = pyglet.resource.image('img/crate.png')
+s_Crate = pyglet.sprite.Sprite(crate_img)
+
 @window.event
 def on_draw():
-    global image_x, image_y
     window.clear()
-    image.blit(image_x, image_y)
-    
+    s_Crate.draw()
+
+def update(dt):
+    global tick
+    if tick == 30:
+        tick = 0
+    tick += 1
+    print(tick)
+
+    if keys[key.W]:
+        if s_Crate.y + 5 > 560:
+            return
+        s_Crate.y += 5
+        print('y: %d x: %d' % (s_Crate.y, s_Crate.x))
+
+    if keys[key.S]:
+        if s_Crate.y - 5 < 0:
+            return
+        s_Crate.y -= 5
+        print('y: %d x: %d' % (s_Crate.y, s_Crate.x))
+
+    if keys[key.A]:
+        if s_Crate.x - 5 < 0:
+            return
+        s_Crate.x -= 5
+        print('y: %d x: %d' % (s_Crate.y, s_Crate.x))
+
+    if keys[key.D]:
+        if s_Crate.x + 5 > 560:
+            return
+        s_Crate.x += 5
+        print('y: %d x: %d' % (s_Crate.y, s_Crate.x))
+
+pyglet.clock.schedule_interval(update, 1/30.)
+
 pyglet.app.run()
